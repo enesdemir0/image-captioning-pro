e5d27cb404939b55f5f4ea1b6a42d3b2f32f0e30
Enes Demir <123deli4567@gmail.com>
2026-04-04 19:10:02 +0300

    fix: implement nested state structure for Keras 3 Stacked LSTM

--- src/models/decoder.py
+++ src/models/decoder.py
@@ -2,15 +2,14 @@ import tensorflow as tf
 
 class RNN_Decoder(tf.keras.Model):
     """
-    A Stacked RNN Decoder (LSTM or GRU) that initializes hidden states 
-    using the Encoder's features.
+    A Stacked RNN Decoder that correctly handles nested states for LSTM/GRU.
     """
     def __init__(self, config):
         super(RNN_Decoder, self).__init__()
         self.units = config['model']['units']
         self.vocab_size = config['dataset']['vocab_size']
         self.embedding_dim = config['model']['embedding_dim']
-        self.cell_type = config['model']['decoder_type'].upper() # Force uppercase
+        self.cell_type = config['model']['decoder_type'].upper()
         self.num_layers = config['model']['num_layers']
         
         # 1. Word Embedding Layer
@@ -23,6 +22,7 @@ class RNN_Decoder(tf.keras.Model):
             cells = [tf.keras.layers.GRUCell(self.units) for _ in range(self.num_layers)]
         
         self.stacked_rnn = tf.keras.layers.StackedRNNCells(cells)
+        # return_state=True returns the nested state structure
         self.rnn = tf.keras.layers.RNN(self.stacked_rnn, return_sequences=True, return_state=True)
 
         # 3. Output Projection
@@ -30,14 +30,13 @@ class RNN_Decoder(tf.keras.Model):
         self.fc2 = tf.keras.layers.Dense(self.vocab_size)
 
     def call(self, x, hidden_state):
-        # Embed the word
+        # x shape: (batch_size, 1)
         x = self.embedding(x)
 
-        # Pass through the RNN stack
-        # next_state will be a list of tensors
-        output, *next_state = self.rnn(x, initial_state=hidden_state)
+        # In Keras 3 Stacked RNN, it returns (output, final_states)
+        # where final_states matches the structure of initial_state
+        output, next_state = self.rnn(x, initial_state=hidden_state)
 
-        # Prediction
         x = self.fc1(output)
         x = tf.reshape(x, (-1, x.shape[2])) 
         logits = self.fc2(x)
@@ -46,13 +45,11 @@ class RNN_Decoder(tf.keras.Model):
 
     def init_decoder_state(self, encoder_output):
         """
-        Initializes the state for every layer.
-        LSTM needs 2 tensors per layer ([h, c, h, c...])
-        GRU needs 1 tensor per layer ([h, h, h...])
+        Creates the correct nested state structure.
         """
         if self.cell_type == "LSTM":
-            # For 3 layers, we need a list of 6 tensors
-            return [encoder_output for _ in range(self.num_layers * 2)]
+            # MUST be a list of lists: [[h, c], [h, c], [h, c]]
+            return [[encoder_output, encoder_output] for _ in range(self.num_layers)]
         else:
-            # For 3 layers, we need a list of 3 tensors
+            # Flat list for GRU: [h, h, h]
             return [encoder_output for _ in range(self.num_layers)]
